6976e00d75f9ce2c1ce40c420e18bade56d7b7a8
mitsato-310326☆ <satou310renrakuyou@gmail.com>
2026-04-10 21:07:06 +0900

    add: put color

--- mandatory/src/main.c
+++ mandatory/src/main.c
@@ -6,19 +6,26 @@
 /*   By: mitsato <mitsato@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/05 20:26:10 by mitsato           #+#    #+#             */
-/*   Updated: 2026/03/26 21:51:20 by mitsato          ###   ########.fr       */
+/*   Updated: 2026/04/10 21:06:26 by mitsato          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-int	main()
+int	main(void)
 {
-	// PERROR
-	t_mlxs *mlxs = init();
+	t_mlxs	*mlxs;
 
+	// PERROR
+	mlxs = init();
 	mlx_hook(mlxs->win, 17, 0, NULL, NULL);
 	mlx_key_hook(mlxs->win, NULL, NULL);
+
+	// for (int n = 0; n <= 1000; n++)
+	// {
+	// 	mlxs->data[n] = (char)255;
+	// }
+
 	print(mlxs);
 	mlx_loop(mlxs->mlx);
 	return (0);
--- mandatory/src/printer/print.c
+++ mandatory/src/printer/print.c
@@ -6,7 +6,7 @@
 /*   By: mitsato <mitsato@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/26 21:30:48 by mitsato           #+#    #+#             */
-/*   Updated: 2026/03/26 21:56:58 by mitsato          ###   ########.fr       */
+/*   Updated: 2026/04/10 20:55:34 by mitsato          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,6 @@ bool print(t_mlxs *mlxs)
 {
 	mlx_put_image_to_window(mlxs->mlx, mlxs->win, mlxs->img, 0, 0);
 	// mlx_string_put(mlxs->mlx, mlxs->mlx, 5, 0, 0xFFFFFF, "Left Click:   Pan");
+	//キーの入力検知（キーフック）からレンダリングを呼べば適した回数のレンダリングになる。
 	return(true);
 }
