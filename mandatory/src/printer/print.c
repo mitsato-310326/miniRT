662b3d4159393c8cd47fa8f66c47409913a52ca7
mitsato-310326☆ <satou310renrakuyou@gmail.com>
2026-04-11 15:53:28 +0900

    add: spell checker

--- mandatory/src/printer/print.c
+++ mandatory/src/printer/print.c
@@ -6,7 +6,7 @@
 /*   By: mitsato <mitsato@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/26 21:30:48 by mitsato           #+#    #+#             */
-/*   Updated: 2026/04/10 20:55:34 by mitsato          ###   ########.fr       */
+/*   Updated: 2026/04/11 15:20:48 by mitsato          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,9 @@
 
 bool print(t_mlxs *mlxs)
 {
+	//first rendering
+	mlx_string_put(mlxs->mlx, mlxs->win, 5, 0, 0xFF00FF, "Left Click:   Pan");
 	mlx_put_image_to_window(mlxs->mlx, mlxs->win, mlxs->img, 0, 0);
-	// mlx_string_put(mlxs->mlx, mlxs->mlx, 5, 0, 0xFFFFFF, "Left Click:   Pan");
-	//キーの入力検知（キーフック）からレンダリングを呼べば適した回数のレンダリングになる。
+
 	return(true);
 }
