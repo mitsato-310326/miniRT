1f5ea8f3b77dd75f0787d0b2c763481e6d2deeca
mitsato-310326☆ <satou310renrakuyou@gmail.com>
2026-06-02 20:52:01 +0900

    add

---
+++ os_setting.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   os_setting.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mitsato <mitsato@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/26 21:51:59 by mitsato           #+#    #+#             */
+/*   Updated: 2026/04/25 17:58:48 by mitsato          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef OS_SETTING_H
+# define OS_SETTING_H
+
+typedef struct s_mlxs	t_mlxs;
+
+/*											*/
+
+/*											*/
+
+typedef enum e_os
+{
+    MAC_OS,
+    LINUX_OS
+} t_os;
+
+#define OS LINUX_OS
+
+/*					DEBUG					*/
+
+/*											*/
+
+#endif
